d6f901e7d5594f002fd9766a1cf3203ac95225bb
Khoa <khoadinh2k@gmail.com>
2023-04-23 22:05:54 -0500

    moved all api relating to foods into one file

--- pages/api/foods.ts
+++ pages/api/foods.ts
@@ -12,14 +12,26 @@ export default async function handler(
   res: NextApiResponse
 ) {
     try {
-      const post: postProps = JSON.parse(req.body);
       if (req.method === 'POST') {
+        const post: postProps = JSON.parse(req.body);
       try {
         const data = await prisma.food.create({
           data: {
             name: post.name,
             desc: post.desc,
             price: post.price,
+          },
+        })
+        res.status(200).json(data); 
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+    } else if (req.method === 'DELETE'){
+      const deleteId = Number(req.query.id);
+      try {
+        const data = await prisma.food.delete({
+          where: {
+            id: deleteId,
           },
         })
         res.status(200).json(data); 

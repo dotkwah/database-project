806c91014772c9418cfb95c1e2a0d5777d81e4d6
Khoa <khoadinh2k@gmail.com>
2023-04-24 02:10:43 -0500

    moved api call functions to seperate files

---
+++ pages/services/drinks.tsx
@@ -0,0 +1,20 @@
+export async function fetchDrinks() {
+  const res = await fetch('/api/drinks', {
+    method: 'GET',
+  });
+  const data = await res.json();
+  return data;
+}
+
+export async function createDrinks(name: string, desc: string, price: number, size: string) {
+  const data = await fetch('/api/drinks', {
+    method: 'POST',
+    body: JSON.stringify({name, desc, price, size}),
+  });
+}
+
+export async function deleteDrinks(id: number) {
+  const data = await fetch('/api/drinks?id=' + id, {
+    method: 'DELETE',
+  });
+}

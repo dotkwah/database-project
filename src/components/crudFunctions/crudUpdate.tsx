dcd1c1cddf79f5e691183f08076dd61421deb7b0
Ethan Wells <ethanmw2000@gmail.com>
2023-04-24 19:46:14 -0500

    updated ui for updating elements

--- src/components/crudFunctions/crudUpdate.tsx
+++ src/components/crudFunctions/crudUpdate.tsx
@@ -7,12 +7,113 @@ import {
   InputAdornment,
   Grid,
   Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+  Switch,
 } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ToggleButtons from "../toggleButtons";
+import { fetchFoods } from "../../../pages/services/foods";
+import { fetchDrinks } from "../../../pages/services/drinks";
+import { fetchSides } from "../../../pages/services/sides";
+
+type Items = {
+  id: number;
+  name: string;
+}
 
 export default function CrudUpdate() {
   const [alignment, setAlignment] = useState('food');
+  const [checked, setChecked] = useState<boolean>(false);
+  const [name, setName] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+  const [size, setSize] = useState<string>("");
+
+  const [foods, setFoods] = useState<Items[]>([]);
+  const [selectedFood, setSelectedFood] = useState<number>();
+
+  const [drinks, setDrinks] = useState<Items[]>([]);
+  const [selectedDrink, setSelectedDrink] = useState<number>();
+
+  const [sides, setSides] = useState<Items[]>([]);
+  const [selectedSide, setSelectedSide] = useState<number>();
+
+  const setSelectedItem = (value: Number) => {
+    if (alignment == "drink") {
+      setSelectedDrink(Number(value));
+    } else if (alignment == "side") {
+      setSelectedSide(Number(value));
+    } else {
+      setSelectedFood(Number(value));
+    }
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  const itemDropdown = () => {
+    if (alignment == "drink") {
+      return drinks.map((drink) => (
+        <MenuItem key={drink.id} value={drink.id}>
+          {drink.name}
+        </MenuItem>
+      ))
+    } else if (alignment == "side") {
+      return sides.map((side) => (
+        <MenuItem key={side.id} value={side.id}>
+          {side.name}
+        </MenuItem>
+      ))
+    } else {
+      return foods.map((food) => (
+        <MenuItem key={food.id} value={food.id}>
+          {food.name}
+        </MenuItem>
+      ))
+    }
+  }
+
+  async function updateItems() {
+    if (alignment == "food") {
+      updateFoods(selectedFood, name, desc, price, checked);
+      setName("")
+      setDesc("")
+      setPrice(0)
+      setChecked(false)
+      fetchFoods();
+    } else if (alignment == "drink"){
+      updateDrinks(selectedDrink, name, desc, price, size);
+      setName("")
+      setDesc("")
+      setPrice(0)
+      setSize("")
+      fetchDrinks();
+    } else if (alignment == "side"){
+      updateSides(selectedSide, desc, price, checked);
+      setName("")
+      setDesc("")
+      setPrice(0)
+      setChecked(false)
+      fetchSides();
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const foodsData = await fetchFoods();
+      const drinksData = await fetchDrinks();
+      const sidesData = await fetchSides();
+      setFoods(foodsData);
+      setDrinks(drinksData);
+      setSides(sidesData);
+    }
+    fetchData();
+  }, []);
 
   return (
     <Container>
@@ -38,17 +139,25 @@ export default function CrudUpdate() {
             />
             </Grid>
             <Grid item xs={12}>
-              <TextField
-                id="id"
-                name="id"
-                label="ID"
-                fullWidth
-              />
+            <FormControl fullWidth>
+            <InputLabel>ID</InputLabel>
+                <Select
+                  labelId="id-select-label"
+                  id="food-select"
+                  value={selectedFood || selectedDrink || selectedSide || ''}
+                  onChange={(e) => setSelectedItem(Number(e.target.value))}
+                  fullWidth
+                >
+                  {itemDropdown()}
+                </Select>
+            </FormControl>
             </Grid>
             <Grid item xs={12}>
               <TextField
                 id="name"
                 name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 label="Name"
                 fullWidth
               />
@@ -58,15 +167,46 @@ export default function CrudUpdate() {
                 id="description"
                 name="description"
                 label="Description"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
                 fullWidth
               />
             </Grid>
+            {alignment === 'drink' ? (
+              <Grid item xs={12}>
+              <TextField
+                  id="size"
+                  name="size"
+                  value={size}
+                  onChange={(e) => setSize(e.target.value)}
+                  label="Size"
+                  fullWidth
+              />
+              </Grid>
+              ) : (
+                <Grid item xs={12}>
+                <Typography 
+                  sx={{ px: 1}} 
+                  fontWeight={600}
+                >
+                  {alignment === 'side' ? 'Vegan' : 'Gluten'}
+                </Typography>
+                <Switch
+                  checked={checked}
+                  onChange={handleChange}
+                  inputProps={{ 'aria-label': 'controlled' }}
+                />
+              </Grid>
+              )}
             <Grid item xs={12}>
-              <OutlinedInput
-                id="price"
-                name="price"
-                fullWidth
-                startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            <OutlinedInput
+                  id="price"
+                  name="price"
+                  value={price}
+                  onChange={(e) => setPrice(Number(e.target.value))}
+                  fullWidth
+                  defaultValue={0}
+                  startAdornment={<InputAdornment position="start">$</InputAdornment>}
               />
             </Grid>
             <Grid item xs={12}>
@@ -75,6 +215,7 @@ export default function CrudUpdate() {
               variant="contained"
               color="primary"
               sx={{my: 2}}
+              onClick={updateItems}
             >
               {"Update"}
             </Button>
